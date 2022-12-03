275e8f38c71ca56d99604f90e62df1190d9d8f7a
firemind <azdemonic@gmail.com>
2022-12-03 10:43:51 +0300

    init

---
+++ certificates/admin.py
@@ -0,0 +1,38 @@
+from django.contrib import admin
+from .models import Events, Templates, Fields
+from django.utils.translation import ugettext_lazy as _
+
+
+class FieldsInline(admin.TabularInline):
+    model = Fields
+
+
+@admin.register(Templates)
+class TemplatesAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (None, {
+            'fields': (
+                'title', 'file',
+            ),
+        }),
+        (_('Header settings'), {
+            'fields': (
+                'headerfont', 'headersize', 'headerln', 'headermtop',
+            ),
+        }),
+        (_('Text settings'), {
+            'fields': (
+                'textfont', 'textsize', 'textln', 'textmtop',
+            ),
+        }),
+    )
+    
+
+@admin.register(Fields)
+class TemplatesAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Events)
+class EventsAdmin(admin.ModelAdmin):
+    inlines = [FieldsInline,]
---
+++ certificates/models.py
@@ -0,0 +1,56 @@
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+
+class Templates(models.Model):
+    title = models.CharField(max_length=50, verbose_name = _('Title'))
+    file = models.FileField(_("Members"), upload_to=None, max_length=100)
+    headerfont = models.CharField(_("Header font"), max_length=50)
+    headersize = models.IntegerField(_("Header size"))
+    headermtop = models.IntegerField(_("Header Margin Top"))
+    headerln = models.IntegerField(_("Header line height"))
+    textfont = models.CharField(_("Text font"), max_length=50)
+    textsize = models.IntegerField(_("Text size"))
+    textmtop = models.IntegerField(_("Text Margin Top"))
+    textln = models.IntegerField(_("Text line height"))
+    
+    class Meta:
+        verbose_name = _("Template")
+        verbose_name_plural = _("Templates")
+
+    def __str__(self):
+        return self.name
+
+    # def get_absolute_url(self):
+    #     return reverse("_detail", kwargs={"pk": self.pk})
+
+
+class Events(models.Model):
+    title = models.CharField(_("Title"), max_length=50)
+    template = models.ForeignKey(Templates, verbose_name=_("Template"), on_delete=models.CASCADE, null=True)
+    userlist = models.FileField(_("Members"), upload_to=None, max_length=100)
+    
+    class Meta:
+        verbose_name = _("Event")
+        verbose_name_plural = _("Events")
+
+    def __str__(self):
+        return self.name
+
+    # def get_absolute_url(self):
+    #     return reverse("Events_detail", kwargs={"pk": self.pk})
+
+
+class Fields(models.Model):
+    template = models.ForeignKey(Events, verbose_name=_("Events"), on_delete=models.CASCADE)
+    text = models.CharField(_("Text"), max_length=200)
+    
+    class Meta:
+        verbose_name = _("Field")
+        verbose_name_plural = _("Fields")
+
+    def __str__(self):
+        return self.name
+
+    # def get_absolute_url(self):
+    #     return reverse("Fields_detail", kwargs={"pk": self.pk})
